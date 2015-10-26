d3f4b57871a910b02d3a599a148503f103d77cd3
Enzo <enzorym@hotmail.com>
2015-10-26 00:48:38 -0300

    ¨nada¨

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=grupo3php',
             'username' => 'root',
-            'password' => 'erym88',
+            'password' => 'root',
             'charset' => 'utf8',
         ],
         'mailer' => [
