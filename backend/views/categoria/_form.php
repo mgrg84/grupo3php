2337cb6a763c34657505a7532862732d1f51d488
Enzo <enzorym@hotmail.com>
2015-11-13 23:53:08 -0300

    guardando imagen del producto

--- backend/views/categoria/_form.php
+++ backend/views/categoria/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'nombre')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'nombre')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
