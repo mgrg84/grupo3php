51dbd2f72db56e77b7c15c40fe0fc589a7030271
Pablo Daniel García Moraes <p.garcia.moraes@gmail.com>
2015-11-23 13:32:56 -0300

    principio ruta api

---
+++ api/modules/v1/controllers/RutaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+
+class RutaController extends ActiveController
+{
+    public $modelClass = 'common\models\Ruta';
+	
+}
