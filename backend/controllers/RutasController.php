dac1fb54465729c4fb480de94116dcdda333948b
Pablo Daniel García Moraes <p.garcia.moraes@gmail.com>
2015-11-26 23:24:57 -0300

    Se agrega exclusion de comercios que ya estan agregados a otras rutas en determinada fecha.

--- backend/controllers/RutasController.php
+++ backend/controllers/RutasController.php
@@ -106,15 +106,29 @@ class RutasController extends Controller
 		$filteredMarkets = [];
 		$user = User::findOne((int)$userID);
 		
-		//var_dump($user);
 		$filterDay = Helpers::GetSpanishDay($date);
 		
 		$userLocation = split(";", $user->ubicacionDomicilio);
+		
+		$routeIDs = Ruta::find()
+			->where("fecha = '".date("y-m-d",  strtotime($date))."'")
+			->select('id')
+			->asArray()
+		;
+		
+		$alreadyAddedMarkets = RutaComercios::find()
+			->where(['idRuta' => $routeIDs])
+			->select('idComercio')	
+			->asArray()
+		;
+		
 		$comercios = Comercio::find()
 			->where($filterDay.' = true ')
+			->andWhere(['NOT IN', 'id', $alreadyAddedMarkets])
 			->orderBy(['prioridad'=>SORT_DESC])
 			->all()
 		;
+		
 		foreach ($comercios as $comercio)
 		{
 			$comercioLocation = split(";", $comercio->ubicacion);
