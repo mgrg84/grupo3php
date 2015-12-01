3b2c7ed4acf7c2ca44bc28e546c1bdf5ebdf271c
EnzoYanes <enzorym@hotmail.com>
2015-12-01 20:16:49 -0300

    intento de pedido en frontend

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -39,6 +39,7 @@ AppAsset::register($this);
         ['label' => 'About', 'url' => ['/site/about']],
         ['label' => 'Contact', 'url' => ['/site/contact']],
     	['label' => 'Recorridos', 'url' => ['/rutas']],
+        ['label' => 'Pedido', 'url' => ['/pedido']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/user/registration/register']];
