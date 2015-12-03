29760b52d4b754a7ef075f5057a5736862f3fabc
SantiagoCallejas <sancagon87@gmail.com>
2015-12-03 12:44:22 -0300

    Se eliminan los .gitignore de las apps

---
+++ common/config/main-local.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=grupo3php',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'useFileTransport' => true,
+        ],
+    ],
+];
